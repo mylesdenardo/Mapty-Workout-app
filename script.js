10f44dfcd6528bad0e007ee1eee204df7ec6b3b0
mylesdenardo <themylesd@gmail.com>
2022-05-10 13:08:18 -0400

    reformatting

--- script.js
+++ script.js
@@ -13,33 +13,47 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 let map, mapEvent;
 
-if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+class App {
+  constructor() {}
 
-      const coords = [latitude, longitude];
+  _getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          const { latitude } = position.coords;
+          const { longitude } = position.coords;
+          console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-      map = L.map('map').setView(coords, 13);
+          const coords = [latitude, longitude];
 
-      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+          map = L.map('map').setView(coords, 13);
 
-      // Handling clicks on map
-      map.on('click', function (mapE) {
-        mapEvent = mapE;
-        form.classList.remove('hidden');
-        inputDistance.focus();
-      });
-    },
-    function () {
-      alert('could not get your position');
-    }
-  );
+          L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution:
+              '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          }).addTo(map);
+
+          // Handling clicks on map
+          map.on('click', function (mapE) {
+            mapEvent = mapE;
+            form.classList.remove('hidden');
+            inputDistance.focus();
+          });
+        },
+        function () {
+          alert('could not get your position');
+        }
+      );
+  }
+
+  _loadMap() {}
+
+  _showForm() {}
+
+  _toggleElevationField() {}
+
+  _newWorkOut() {}
+}
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
